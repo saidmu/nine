080b83fde7ae15e6b01f34c51ebb817ca3b4aa7f
Mu <mujianmi@amazon.com>
2021-03-04 09:55:56 +0800

    restructured dynamodb file

--- awslib/awsdynamodb/dynamo.go
+++ awslib/awsdynamodb/dynamo.go
@@ -3,15 +3,57 @@ package awsdynamodb
 import (
 	"context"
 	"errors"
-
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
+// DyPutItemAPI func
+type DyPutItemAPI interface {
+	PutItem(ctx context.Context, params *dynamodb.PutItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error)
+}
+
+type DYUpdateItemAPI interface {
+	UpdateItem(ctx context.Context, params *dynamodb.UpdateItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.UpdateItemOutput, error)
+}
+
 // DYQueryAPI used to prepare unit test
 type DYQueryAPI interface {
 	Query(ctx context.Context, params *dynamodb.QueryInput, optFns ...func(*dynamodb.Options)) (*dynamodb.QueryOutput, error)
+}
+
+// PutItem defines common methods for putting data into dynamodb
+func PutItem(ctx context.Context, Client DyPutItemAPI, table *string, data DYPayload) error {
+	marshaledData, err := data.DynamodbData()
+	if err != nil {
+		return err
+	}
+	input := &dynamodb.PutItemInput{Item: marshaledData, TableName: table}
+	_, err = Client.PutItem(ctx, input)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateItem(ctx context.Context,Client DYUpdateItemAPI,table *string,key map[string]types.AttributeValue,field expression.UpdateBuilder) error {
+	expr,err := expression.NewBuilder().WithUpdate(field).Build()
+	if err != nil {
+		return err
+	}
+	input := &dynamodb.UpdateItemInput{
+		Key: key,
+		ExpressionAttributeNames: expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		UpdateExpression: expr.Update(),
+		TableName: table,
+	}
+	_, err = Client.UpdateItem(ctx,input)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // AllQuery will accept at  most one filter
@@ -19,7 +61,7 @@ func AllQuery(ctx context.Context, client DYQueryAPI, table *string, keyConditio
 	var output []map[string]types.AttributeValue
 	var input *dynamodb.QueryInput
 	if f := len(filter); f > 1 {
-		return nil, errors.New("Only accept on filter condition")
+		return nil, errors.New("only accept on filter condition")
 	} else if f == 0 {
 		expr, err := expression.NewBuilder().WithKeyCondition(keyCondition).Build()
 		if err != nil {

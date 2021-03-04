658ffd8d8d4d399a13a00769d8bc89d5456f3727
Mu <mujianmi@amazon.com>
2021-03-04 09:48:23 +0800

    delete search specific redis cluster function

--- awslib/awsredis/awsredis.go
+++ awslib/awsredis/awsredis.go
@@ -14,32 +14,6 @@ type FailoverGlobalReplicationGroupAPI interface {
 	FailoverGlobalReplicationGroup(ctx context.Context, params *elasticache.FailoverGlobalReplicationGroupInput, optFns ...func(*elasticache.Options)) (*elasticache.FailoverGlobalReplicationGroupOutput, error)
 }
 
-// GetAllRedisReplicationGroup return map of groupdid and globalreplicationgroupid
-func GetAllRedisReplicationGroup(client DescribeReplicationGroupsAPI) (map[string]string, error) {
-	output := make(map[string]string)
-	input := &elasticache.DescribeReplicationGroupsInput{}
-	for {
-		result, err := client.DescribeReplicationGroups(context.TODO(), input)
-		if err != nil {
-			return nil, err
-		}
-		for _, item := range result.ReplicationGroups {
-			if item.GlobalReplicationGroupInfo == nil {
-				continue
-			}
-			if *item.GlobalReplicationGroupInfo.GlobalReplicationGroupMemberRole != "SECONDARY" {
-				continue
-			}
-			output[*item.ReplicationGroupId] = *item.GlobalReplicationGroupInfo.GlobalReplicationGroupId
-		}
-		if result.Marker == nil {
-			break
-		}
-		input.Marker = result.Marker
-	}
-	return output, nil
-}
-
 // PromoteRedisSecondaryToPrimary
 func PromoteRedisSecondaryToPrimary(client FailoverGlobalReplicationGroupAPI, region string, pid,gid *string) error {
 	input := &elasticache.FailoverGlobalReplicationGroupInput{

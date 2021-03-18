4f5c33901deb2a09df49d35a7f815cc9ea4b622a
Mu <mujianmi@amazon.com>
2021-03-18 15:15:12 +0800

    new echo limit concurrency middleware

---
+++ midecho/concurrency.go
@@ -0,0 +1,25 @@
+package midecho
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type ConcurConfig struct {
+	Queue chan struct{}
+}
+
+func (c ConcurConfig) Limit(next echo.HandlerFunc) echo.HandlerFunc  {
+	select {
+	case c.Queue <- struct{}{}:
+		defer func() { <-c.Queue }()
+		return func(c echo.Context) error {
+			return next(c)
+		}
+	default:
+		return func(c echo.Context) error {
+			c.JSON(http.StatusTooManyRequests,echo.Map{"Message": "Too many requests"})
+			return nil
+		}
+	}
+}
